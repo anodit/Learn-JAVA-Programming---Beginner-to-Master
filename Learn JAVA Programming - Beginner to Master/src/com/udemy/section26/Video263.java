96b510a8c867e7443e776929522f79c12f6f52db
anodit <mohitkt2247@gmail.com>
2023-04-01 18:14:20 +0530

    video 263 code added

--- Learn JAVA Programming - Beginner to Master/src/com/udemy/section26/Video263.java
+++ Learn JAVA Programming - Beginner to Master/src/com/udemy/section26/Video263.java
@@ -1,10 +1,28 @@
 package com.udemy.section26;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 public class Video263 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Video 263");
+		//Joda Time API
+		Date d = new Date();
+		d.setHours(21); // It is mutable
+		System.out.println(d);
+		
+//		LocalDate dt = LocalDate.now();
+//		System.out.println(dt);
+		LocalDate dt = LocalDate.of(2022, 04, 22);
+		dt = dt.plusYears(1); // It is immutable
+		System.out.println(dt);
+		
+		LocalTime lt = LocalTime.now();
+		System.out.println(lt);
 	}
 
 }
